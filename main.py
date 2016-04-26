084c61548eec01b34953253697f791e3fcb87b2d
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-04-26 17:45:16 +0200

    fix: use print instead of sys.stdout.write

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ from keboola import docker
 cfg = docker.Config()
 
 if (cfg.get_action() == 'test'):
-    sys.stdout.write('test')
+    print('test')
     sys.exit(0)
 elif (cfg.get_action() == 'timeout'):
     time.sleep(60)
     sys.exit(0)
 elif (cfg.get_action() == 'json'):
-    sys.stdout.write('{"tables": ["a", "b", "c"]}')
+    print('{"tables": ["a", "b", "c"]}')
     sys.exit(0)
 elif (cfg.get_action() == 'invalidjson'):
-    sys.stdout.write('{"tables": ["a", "b", "c"]')
+    print('{"tables": ["a", "b", "c"]')
     sys.exit(0)
 elif (cfg.get_action() == 'noresponse'):
     sys.exit(0)
 elif (cfg.get_action() == 'usererror'):
-    sys.stdout.write('user error')
+    print('user error')
     sys.exit(1)
 elif (cfg.get_action() == 'apperror'):
-    sys.stdout.write('application error')
+    print('application error')
     sys.exit(2)
 elif (cfg.get_action() == 'encrypt'):
     params = cfg.get_parameters()
     if (params.get('#password') == 'password'):
-        sys.stdout.write('success')
+        print('success')
     else:
-        sys.stdout.write('failed')
+        print('failed')
         sys.exit(1)
 else:
-    sys.stdout.write('undefined action' + cfg.get_action())
+    print('undefined action' + cfg.get_action())
     sys.exit(2)
